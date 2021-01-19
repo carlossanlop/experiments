f283ba1047848e6a8118674c6259be34811711d6
carlossanlop <carlossanlop@users.noreply.github.com>
2021-01-19 13:39:56 -0800

    Make src a console app

---
+++ RedundantSegments/src/MainClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RedundantSegments
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"Attach dotnet trace to pid {Environment.ProcessId}. Running loop...");
+            while (true)
+            {
+                MyPath.GetFullPath("/this/is/a/path/without/redundant/segments");
+            }
+        }
+    }
+}
